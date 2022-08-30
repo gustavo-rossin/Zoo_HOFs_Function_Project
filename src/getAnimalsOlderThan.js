5b295b885f56c12fca7665b2e4af77b31435f911
gustavo-rossin <gustavo.rossin@hotmail.com>
2022-08-30 16:28:30 -0300

    challenge 2 completed

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const findAnimal = data.species.find((specie) => specie.name === animal).residents;
+
+  const booleanSpeciesAge = findAnimal.every((aging) => aging.age >= age);
+
+  return booleanSpeciesAge;
 }
+
+console.log(getAnimalsOlderThan('otters', 7));
+console.log(getAnimalsOlderThan('penguins', 10));
 
 module.exports = getAnimalsOlderThan;
