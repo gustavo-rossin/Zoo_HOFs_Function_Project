b159ba6f0e815128033a73fae76bcafa1228054b
gustavo-rossin <gustavo.rossin@hotmail.com>
2022-08-30 15:10:36 -0300

    challenge 1 done

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  return ids.map((stringId) => data.species.find((animal) => animal.id === stringId));
 }
+
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
+
+console.log(getSpeciesByIds());
 
 module.exports = getSpeciesByIds;
