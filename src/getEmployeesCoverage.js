e974e16eeb755d64d01b05693b1a4e2cc5d70fc6
gustavo-rossin <gustavo.rossin@hotmail.com>
2022-09-02 13:03:04 -0300

    checking throw error on challenge 11

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -2,78 +2,41 @@ const data = require('../data/zoo_data');
 
 const { species, employees } = data;
 
-const workers = [
-  {
-    id: 'c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1',
-    fullName: 'Nigel Nelson',
-    species: ['lions', 'tigers'],
-    locations: ['NE', 'NW'],
-  },
-  {
-    id: '0e7b460e-acf4-4e17-bcb3-ee472265db83',
-    fullName: 'Burl Bethea',
-    species: ['lions', 'tigers', 'bears', 'penguins'],
-    locations: ['NE', 'NW', 'NW', 'SE'],
-  },
-  {
-    id: 'fdb2543b-5662-46a7-badc-93d960fdc0a8',
-    fullName: 'Ola Orloff',
-    species: ['otters', 'frogs', 'snakes', 'elephants'],
-    locations: ['SE', 'SW', 'SW', 'NW'],
-  },
-  {
-    id: '56d43ba3-a5a7-40f6-8dd7-cbb05082383f',
-    fullName: 'Wilburn Wishart',
-    species: ['snakes', 'elephants'],
-    locations: ['SW', 'NW'],
-  },
-  {
-    id: '9e7d4524-363c-416a-8759-8aa7e50c0992',
-    fullName: 'Stephanie Strauss',
-    species: ['otters', 'giraffes'],
-    locations: ['SE', 'NE'],
-  },
-  {
-    id: '4b40a139-d4dc-4f09-822d-ec25e819a5ad',
-    fullName: 'Sharonda Spry',
-    species: ['otters', 'frogs'],
-    locations: ['SE', 'SW'],
-  },
-  {
-    id: 'c1f50212-35a6-4ecd-8223-f835538526c2',
-    fullName: 'Ardith Azevado',
-    species: ['tigers', 'bears'],
-    locations: ['NW', 'NW'],
-  },
-  {
-    id: 'b0dc644a-5335-489b-8a2c-4e086c7819a2',
-    fullName: 'Emery Elser',
-    species: ['lions', 'bears', 'elephants'],
-    locations: ['NE', 'NW', 'NW'],
-  },
-];
-
 const getSpecies = ([...id]) => id.map((arr) => species.find((fil) => arr === fil.id).name);
 
 const getLocationById = ([...id]) => id.map((arr) => species
   .find((fil) => arr === fil.id).location);
 
+const workersInfo = () => employees.map((arr) => ({
+  id: arr.id,
+  fullName: `${arr.firstName} ${arr.lastName}`,
+  species: getSpecies(arr.responsibleFor),
+  locations: getLocationById(arr.responsibleFor),
+}));
+
 function getEmployeesCoverage(employee) {
-  const findEmployee = employees
-    .find((el1) => el1.id === employee || el1.firstName === employee || el1.lastName === employee);
-  if (employee === undefined) {
-    return workers;
+  if (!employee) {
+    return workersInfo();
   }
-  return {
+  const findEmployee = employees.find((string) => string.id === employee.id
+    || string.firstName === employee.name
+    || string.lastName === employee.name);
+  if (!findEmployee) {
+    throw new Error('Informações inválidas');
+  }
+  const coverage = {
     id: findEmployee.id,
     fullName: `${findEmployee.firstName} ${findEmployee.lastName}`,
     species: getSpecies(findEmployee.responsibleFor),
     locations: getLocationById(findEmployee.responsibleFor),
   };
+  if (findEmployee) {
+    return coverage;
+  }
 }
 // console.log(getEmployeesCoverage());
-console.log(getEmployeesCoverage('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
-// console.log(getEmployeesCoverage('Nigel'));
-// console.log(getEmployeesCoverage('Nelson'));
-
+// console.log(getEmployeesCoverage({ name: 'Sharonda' }));
+// console.log(getEmployeesCoverage({ name: 'Nelson' }));
+// console.log(getEmployeesCoverage({ id: 'c1f50212-35a6-4ecd-8223-f835538526c2' }));
+console.log(getEmployeesCoverage({ id: 'Id inválido' }));
 module.exports = getEmployeesCoverage;
